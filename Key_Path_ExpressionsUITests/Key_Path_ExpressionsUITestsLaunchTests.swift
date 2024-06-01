e9f5bcca37cf2fbf1090459fd8c06dfb6c432916
Raja <Rajasekhar.iosswift@gmail.com>
2024-06-01 10:19:56 +0530

    Initial Commit

---
+++ Key_Path_ExpressionsUITests/Key_Path_ExpressionsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Key_Path_ExpressionsUITestsLaunchTests.swift
+//  Key_Path_ExpressionsUITests
+//
+//  Created by rajasekhar pidikiti on 01/06/24.
+//
+
+import XCTest
+
+final class Key_Path_ExpressionsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
